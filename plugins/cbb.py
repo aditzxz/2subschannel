2f4b33bde2de49945362cdad3583e97e9536f6c6
aditzxz <96695404+aditzxz@users.noreply.github.com>
2022-09-06 00:18:21 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Oura-Ubot/Oura-XFsub-Bot1'>Click here</a>\n○ Channel : @OuraaProject\n○ Owner : @Owaitingforyou</b>",
+            text = f"<b>○ bokep vip ready : <a href='tg://user?id={OWNER_ID}'>chat @oliviaxyx</a>\n○ vip group : https://t.me/viptesti/187 <a href=' here</a>\n○ Channel vip : https://t.me/viptesti</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
